1569964ebf3527a47a32ff30327e0757b8c004d7
Edson Júnior <ebf.junin@gmail.com>
2018-05-06 14:27:39 -0300

    Added status field

--- src/model/cart.rb
+++ src/model/cart.rb
@@ -10,8 +10,14 @@ class Cart < Record
   include Mongoid::Timestamps::Created
   include Mongoid::Timestamps::Updated
 
+  field :status, type: Integer, default: 0
+
   embeds_one :buyer
   embeds_one :seller
   embeds_many :products
   accepts_nested_attributes_for :buyer, :seller, :products
+
+  scope :open, -> { where(status: 0) }
+  scope :paid, -> { where(status: 1) }
+  scope :expired, -> { where(status: 2) }
 end
